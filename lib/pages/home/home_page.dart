4cb943c71cfb0b680bd21308d01fc5cd85d31025
waitwalker <waitwalker@163.com>
2019-12-25 10:06:15 +0800

    主页面创建

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    _HomeState();
+  }
+}
+
+class _HomeState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
